b3328e4f3747f1bc5fe9520edb7ac694d53f6089
personary <apersonary@gmail.com>
2015-01-25 18:43:10 -0500

    first commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Joshua Haines on 1/23/15.
+//  Copyright (c) 2015 Joshua Haines. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
